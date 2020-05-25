4393ed78359edd81690042688d814ea0870b25bc
abhishek verma <abhishek.abhiaies99@gmail.com>
2020-05-25 13:56:01 +0530

    GameFinalized and Updated on Google Play Store

--- Assets/Scripts/LevelController/LevelMenuScript.cs
+++ Assets/Scripts/LevelController/LevelMenuScript.cs
@@ -8,27 +8,24 @@ using UnityEngine.SceneManagement;
 public class LevelMenuScript : MonoBehaviour
 {
 
-    //[Header("Showig Banner Ads on Game Over ")]
-    //private AdManager admanager;
+     
 
     private void Awake()
     {
-     //   admanager = GameObject.Find("AdManager").GetComponent<AdManager>();
+ 
     }
 
     public void Level1Load()
     {
-      //  AdManager.Instance.ShowRewardedVideo();
+         AdManager.Instance.ShowRewardedVideo();
         Time.timeScale = 1f;
         SceneManager.LoadSceneAsync("Play_Opp");
         Time.timeScale = 1f;
     }
     public void Level2Load()
     {
-       // AdManager.Instance.ShowRewardedVideo();
+        AdManager.Instance.ShowRewardedVideo();
         Time.timeScale = 1f;
-
-        //admanager.ShowRewardedVideo();
         SceneManager.LoadSceneAsync("Learn");
         Time.timeScale = 1f;
     }
--- Assets/Scripts/MainMenuController/MainMenuController.cs
+++ Assets/Scripts/MainMenuController/MainMenuController.cs
@@ -5,50 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-   // [Header("Showig Banner Ads on Main Menu ")]
-   // private AdManager admanager;
-
+    
 
     private void Awake()
     {
-    //    admanager = GameObject.Find("AdManager").GetComponent<AdManager>();
+        AdManager.Instance.ShowBanner();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-       // AdManager.Instance.ShowBanner();
+        AdManager.Instance.ShowBanner();
         
     }
     public void ShowBannerAdonMainMenu()
     {
-        if (SceneManager.GetActiveScene().buildIndex == 0)
-        {
-            Debug.Log("Main Menu");
-           // admanager.ShowBanner();
-
-        }
+         
     }
 
     public void RemoveBannerAdonLevelMenu()
     {
-        if (SceneManager.GetActiveScene().buildIndex ==2)
-        {
-            Debug.Log("Level Menu");
-           // admanager.DestroyBanner();
-
-        }
+        
     }
 
 
     private void Update()
     {
-        ShowBannerAdonMainMenu();
-        RemoveBannerAdonLevelMenu();
+        
     }
     public void LoadtheLevelMenu()
     {
-       // AdManager.Instance.ShowBanner();
+       AdManager.Instance.ShowBanner();
         SceneManager.LoadSceneAsync("LevelMenu");
     }
 }
